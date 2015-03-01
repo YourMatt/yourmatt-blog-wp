299929598dd8d33d6eb2054f61a6b6fb73093c0f
YourMatt <github@yourmatt.com>
2015-02-28 17:05:39 -0700

    Change header links depending on whether single page or list page.

--- wp-content/themes/yourmatt2015/header.php
+++ wp-content/themes/yourmatt2015/header.php
@@ -21,6 +21,14 @@
         </div>
         <div class="stripe"><div class="detail"></div></div>
         <ul class="navigation">
+            <?  // add navigation for single page
+                if (is_single ()) : ?>
+            <li><a href="/older">Older</a></li>
+            <li><a href="/newer">Newer</a></li>
+            <li><a href="/random">Random</a></li>
+            <li class="separator"></li>
+            <?  // add navigation for all list pages
+                else : ?>
             <li class="selected"><a href="/2015">2015</a></li>
             <li class="separator"></li>
             <li><a href="/2011">2011</a></li>
@@ -28,6 +36,7 @@
             <li><a href="/2009">2009</a></li>
             <li><a href="/2008">2008</a></li>
             <li><a href="/2007">2007</a></li>
+            <? endif; ?>
             <li class="search">
                 <form id="search-form">
                     <input type="text" value="" name="search"/>
