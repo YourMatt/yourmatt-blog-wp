e1a625ee19c4923f0199a033ad3d913bd0cbd1bb
YourMatt <github@yourmatt.com>
2015-03-06 19:11:47 -0700

    Set image scaling to apply after images load.

--- wp-content/themes/yourmatt2015/js/containerfills.js
+++ wp-content/themes/yourmatt2015/js/containerfills.js
@@ -24,35 +24,39 @@ var containerFills = {
       var base = this;
 
       $(".fill-scaled > img").each (function () {
-
-         // gather base attributes of the container and containing image
-         var container = base.getObjectAttributes ($(this).parent ());
-         var image = base.getObjectAttributes ($(this));
-         if (! image.width) return;
-
-         // set the position attributes
-         var newPositions = {};
-         if (container.aspectRatio < image.aspectRatio) {
-            newPositions.width = container.height * image.aspectRatio;
-            newPositions.height = container.height;
-            newPositions.left =  - (newPositions.width - container.width) / 2;
-            newPositions.top = 0;
-         }
-         else {
-            newPositions.width = container.width;
-            newPositions.height = container.width / image.aspectRatio;
-            newPositions.left = 0;
-            newPositions.top = - (newPositions.height - container.height) / 2;
-         }
-
-         $(this).css ({
-            marginLeft: newPositions.left,
-            marginTop: newPositions.top
-         });
-         $(this).width (newPositions.width);
-         $(this).height (newPositions.height);
-
+         base.fillImageScaled ($(this));
       });
+
+   },
+
+   fillImageScaled: function (imageTag) {
+
+      // gather base attributes of the container and containing image
+      var container = this.getObjectAttributes (imageTag.parent ());
+      var image = this.getObjectAttributes (imageTag);
+      if (! image.width) return;
+
+      // set the position attributes
+      var newPositions = {};
+      if (container.aspectRatio < image.aspectRatio) {
+         newPositions.width = container.height * image.aspectRatio;
+         newPositions.height = container.height;
+         newPositions.left =  - (newPositions.width - container.width) / 2;
+         newPositions.top = 0;
+      }
+      else {
+         newPositions.width = container.width;
+         newPositions.height = container.width / image.aspectRatio;
+         newPositions.left = 0;
+         newPositions.top = - (newPositions.height - container.height) / 2;
+      }
+
+      imageTag.css ({
+         marginLeft: newPositions.left,
+         marginTop: newPositions.top
+      });
+      imageTag.width (newPositions.width);
+      imageTag.height (newPositions.height);
 
    },
 
@@ -76,7 +80,14 @@ var containerFills = {
 };
 function containerFillsReset () {
    containerFills.setImageFills ();
+
+   // set the individual image area to be centered after loading
+   $(".fill-scaled > img").load (function () {
+      console.log ($(this).width() + " x " + $(this).height() + " - " + $(this).attr("src"));
+      containerFills.fillImageScaled ($(this));
+   });
 }
 
 $(document).ready (containerFillsReset);
 $(window).resize (containerFillsReset);
+
